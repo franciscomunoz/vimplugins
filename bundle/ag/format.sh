0e43a00746e0aa30690a413ab55ab06faaa70565
Francisco Munoz <francisco.munoz.ruiz@intel.com>
2015-12-28 13:56:00 -0600

    Changed curent plugin repos for git submodules

--- bundle/ag/format.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-CLANG_FORMAT=clang-format-3.5
-
-$CLANG_FORMAT -i src/*.c
--- bundle/ag/tests/setup.sh
+++
@@ -1,4 +0,0 @@
-# All cram tests should use this. Make sure that "ag" runs the version
-# of ag we just built, and make the output really simple.
-
-alias ag="$TESTDIR/../ag --nocolor --workers=1 --parallel"
